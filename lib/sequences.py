dd4f4d5e039392bbc6bb1f0e96f54f261151120c
NatalieGichuki <nataliegichuki@gmail.com>
2025-05-07 15:33:51 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    sequence = [0, 1]
+    while len(sequence) < length:
+        next_value = sequence[-1] + sequence[-2]
+        sequence.append(next_value)
+    
+    print(sequence)
+            
     pass
